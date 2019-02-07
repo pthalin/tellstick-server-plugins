abee8c7f35852b2d6cfbe95ba34ac9e30c264fc1
Patrik Thalin <patrik@thalin.se>
2019-02-07 10:12:50 +0100

    Support for multiple devices

--- airthings/server/server.py
+++ airthings/server/server.py
@@ -1,11 +1,11 @@
-from bluepy.btle import UUID, Peripheral
+from bluepy.btle import UUID, Peripheral, Scanner, DefaultDelegate
 from datetime import datetime
 from wsgiref.simple_server import make_server
 import sys
 import time
 import struct
 import re
-
+import json
 
 class Sensor:
     def __init__(self, name, uuid, format_type, unit, scale):
@@ -15,31 +15,44 @@ class Sensor:
         self.unit = unit
         self.scale = scale
 
+
 def get_data():
     str_out = ""        
     try:
         sensors = []
+        data = {}
         sensors.append(Sensor("DateTime", UUID(0x2A08), 'HBBBBB', "\t", 0))
         sensors.append(Sensor("Temperature", UUID(0x2A6E), 'h', "deg C\t", 1.0/100.0))
         sensors.append(Sensor("Humidity", UUID(0x2A6F), 'H', "%\t\t", 1.0/100.0))
         sensors.append(Sensor("Radon 24h avg", "b42e01aa-ade7-11e4-89d3-123b93f75cba", 'H', "Bq/m3\t", 1.0))
         sensors.append(Sensor("Radon long term", "b42e0a4c-ade7-11e4-89d3-123b93f75cba", 'H', "Bq/m3\t", 1.0))
 
-        addr =  "98:07:2d:05:bf:2f"
-        p = Peripheral(addr)
+        for key in devs.keys():
+            data[key] = {}
+            addr = str(devs[key])
+            print addr
+            p = Peripheral(addr)
 
 
-        for s in sensors:
-            ch  = p.getCharacteristics(uuid=s.uuid)[0]
-            if (ch.supportsRead()):
-                val = ch.read()
-                val = struct.unpack(s.format_type, val)
-                if s.name == "DateTime":
-                    str_out += str(datetime(val[0], val[1], val[2], val[3], val[4], val[5])) + ","
-                else:
-                    str_out += str(val[0] * s.scale) + ","
-    
-        str_out += addr
+            for s in sensors:
+                ch  = p.getCharacteristics(uuid=s.uuid)[0]
+                if (ch.supportsRead()):
+                    val = ch.read()
+                    val = struct.unpack(s.format_type, val)
+
+                    if s.name == "DateTime":
+                        str_out += str(datetime(val[0], val[1], val[2], val[3], val[4], val[5])) + ","
+                        data[key][s.name] = str(datetime(val[0], val[1], val[2], val[3], val[4], val[5]))
+                    else:
+                        str_out += str(val[0] * s.scale) + ","
+                        data[key][s.name] = str(val[0] * s.scale)
+                        
+            str_out += addr
+            data[key]['MAC'] = addr
+            str_out += ","
+            if 'p' in locals():
+                p.disconnect()
+        str_out += "END"        
     except Exception as e:
 	print("Could not request Wave value. %s" %  e)
         str_out = ""
@@ -47,8 +60,28 @@ def get_data():
     finally:
         if 'p' in locals():
             p.disconnect()
-            
-    return str_out 
+
+    #print(data)
+    json_data = json.dumps(data)
+
+    print(json_data)
+    return json_data; 
+    #return str_out 
+
+def parseSerialNumber(ManuDataHexStr):
+    if (ManuDataHexStr == "None"):
+        SN = "Unknown"
+    else:
+        ManuData = bytearray.fromhex(ManuDataHexStr)
+
+        if (((ManuData[1] << 8) | ManuData[0]) == 0x0334):
+            SN  =  ManuData[2]
+            SN |= (ManuData[3] << 8)
+            SN |= (ManuData[4] << 16)
+            SN |= (ManuData[5] << 24)
+        else:
+            SN = "Unknown"
+    return SN
 
 
 def application(environ, start_response):
@@ -56,5 +89,24 @@ def application(environ, start_response):
     retval =  get_data()
     print(retval)
     return retval
+
+def findDevs():
+    devs = {}
+    scanner     = Scanner().withDelegate(DefaultDelegate())
+    searchCount = 0
+    while searchCount < 20:
+        devices      = scanner.scan(0.2) # 0.1 seconds scan period
+        searchCount += 1
+        for dev in devices:
+            ManuData = dev.getValueText(255)
+            SN = parseSerialNumber(ManuData)
+            if (SN != "Unknown"):
+                    devs[SN] = dev.addr        
+    return devs
+
+devs = findDevs()
+
+for key in devs.keys():
+    print key, devs[key]
 
 make_server('', 8888, application).serve_forever()
--- airthings/src/radon/Radon.py
+++ airthings/src/radon/Radon.py
@@ -10,21 +10,20 @@ from telldus import DeviceManager, Sensor
 
 
 class RadonSensor(Sensor):
-	def __init__(self, sensorId, serial):
+	def __init__(self, sensorId):
 		super(RadonSensor,self).__init__()
 		self.sensorId = sensorId
-                self.serial = serial
 		self.setName('Radon 24h')
 
 	def localId(self):
 		return self.sensorId
 
         def model(self):
-		return self.serial
+		return 'wave'
 
 	def typeString(self):
 		return 'radon'
-                
+
 @configuration(
 	serverAddress = ConfigurationString(
 		defaultValue='192.168.1.164:8888',
@@ -38,8 +37,11 @@ class RadonSensor(Sensor):
 class Radon(Plugin):
 	def __init__(self):
 		self.deviceManager = DeviceManager(self.context)
-		self.sensor = None
+		self.sensors = {}
+		#self.deviceManager.removeDevicesByType('radon')
             	Application().registerScheduledTask(self.updateValues, minutes=5, runAtOnce=True)
+
+
 
 	def updateValues(self):
                 host_url = 'http://' + self.config('serverAddress')
@@ -52,33 +54,48 @@ class Radon(Plugin):
                 for idx in x:
                         macStr = ""
                         for key in x[idx]:
-                                #logging.info('idx %s', idx)
-                                #logging.info('    key %s', key)
-                                #logging.info('       val %s', x[idx][key])
                                 if  key == 'Temperature':
                                         temperature = round(float(x[idx][key]),1)
                                 elif key == 'Humidity':
-                                    humidity = round(float(x[idx][key]))
+                                        humidity = round(float(x[idx][key]))
                                 elif key == 'Radon 24h avg':
                                         radon24h = round(float(x[idx][key]))
+                                elif key == 'Radon long term':
+                                        radonlong = round(float(x[idx][key]))
                                 elif key == 'MAC':
                                         macStr = str(x[idx][key])
-                                
+                                elif key == 'DateTime':
+                                        datetime = str(x[idx][key])
+                                else:
+                                        logging.info('Unknown key: %s', key)
+                                        
                         if macStr:
-                                idNum = 0x000000FFFFFF & int(macStr.translate(None, ":"), 16)
+                                #idNum = int(idx)
+                                idNum       = 0x000000FFFFFF & int(macStr.translate(None, ":"), 16)
                                 logging.info('Radon 24h is %f', radon24h)
+                                logging.info('Radon long is %f', radonlong)
                                 logging.info('Humidity is %f', humidity)
                                 logging.info('Temperature is %f', temperature)
                                 logging.info('MAC Adddress is %s', macStr)
+                                logging.info('DateTime is %s', datetime)
                                 logging.info('SN is %s', idx)
                                 logging.info('id Num is %d', idNum)
 
-                	        self.sensor = RadonSensor(idNum, 'S/N:' + str(idx))
-                                self.sensor.setSensorValue(Sensor.WATT, radon24h, Sensor.SCALE_POWER_WATT)
-                                self.sensor.setSensorValue(Sensor.TEMPERATURE, temperature, Sensor.SCALE_TEMPERATURE_CELCIUS)
-                                self.sensor.setSensorValue(Sensor.HUMIDITY, humidity, Sensor.SCALE_HUMIDITY_PERCENT)
-			        self.deviceManager.addDevice(self.sensor)
-			        self.deviceManager.finishedLoading('radon')
+                                if idNum not in self.sensors:
+                                        logging.info('Added new sendsor %d', idNum)
+                	                self.sensors[idNum] = RadonSensor(idNum)
+                                        self.sensors[idNum].setSensorValue(Sensor.WATT, radon24h, Sensor.SCALE_POWER_WATT)
+                                        #self.sensors[idNum].setSensorValue(Sensor.WATT, radonlong, Sensor.SCALE_POWER_VOLT)
+                                        self.sensors[idNum].setSensorValue(Sensor.TEMPERATURE, temperature, Sensor.SCALE_TEMPERATURE_CELCIUS)
+                                        self.sensors[idNum].setSensorValue(Sensor.HUMIDITY, humidity, Sensor.SCALE_HUMIDITY_PERCENT)
+                                        self.deviceManager.addDevice(self.sensors[idNum])
+			                self.deviceManager.finishedLoading('radon')
+		                else:
+                                        logging.info('Updated sendsor %d', idNum)
+                                        self.sensors[idNum].setSensorValue(Sensor.WATT, radon24h, Sensor.SCALE_POWER_WATT)
+                                        #self.sensors[idNum].setSensorValue(Sensor.WATT, radonlong, Sensor.SCALE_POWER_VOLT)
+                                        self.sensors[idNum].setSensorValue(Sensor.TEMPERATURE, temperature, Sensor.SCALE_TEMPERATURE_CELCIUS)
+                                        self.sensors[idNum].setSensorValue(Sensor.HUMIDITY, humidity, Sensor.SCALE_HUMIDITY_PERCENT)
                         else:
                                 logging.info('Error: server did not provide response. Make sure your device is in range and that the server has the correct MAC address.')
                 
